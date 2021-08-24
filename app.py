6b94928241e2700d63942ffc5561c032c0c8bc09
Patrick_Liao <patrick@workos.com>
2021-08-24 10:46:34 -0500

    defined all params and updated get_profile to get_profile_and_token

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ workos.base_api_url = 'http://localhost:7000/' if DEBUG else workos.base_api_url
 # There'd realistically be persons with different domains trying to sign in,
 # where some may have SSO and some may not. This example assumes only workos.com
 # domains with SSO setup.
-CUSTOMER_EMAIL_DOMAIN = 'gmail.com'
+CUSTOMER_EMAIL_DOMAIN = 'example.com'
+CUSTOMER_CONNECTION_ID = 'connection_id here'
 
 @app.route('/')
 def login():
@@ -25,9 +26,10 @@ def login():
 @app.route('/auth')
 def auth():
     authorization_url = workos.client.sso.get_authorization_url(
-        CUSTOMER_EMAIL_DOMAIN,
-        url_for('auth_callback', _external=True),
-        state={}
+        domain = CUSTOMER_EMAIL_DOMAIN,
+        redirect_uri = url_for('auth_callback', _external=True),
+        state = {},
+        connection = CUSTOMER_CONNECTION_ID
     )
 
     return redirect(authorization_url)
@@ -35,6 +37,7 @@ def auth():
 @app.route('/auth/callback')
 def auth_callback():
     code = request.args.get('code')
-    profile = workos.client.sso.get_profile(code)
+    print(code)
+    profile = workos.client.sso.get_profile_and_token(code)
 
     return profile.to_dict()
